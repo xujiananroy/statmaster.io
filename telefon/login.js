899f3497e386599ab21973860438793f5b744af0
xujiananroy <xujiananroy@gmail.com>
2024-10-31 08:44:50 +0100

    update

---
+++ telefon/login.js
@@ -0,0 +1,24 @@
+function registration_div() {
+    var registration_elements = document.getElementsByClassName("registration");
+    var login_elements = document.getElementsByClassName("login");
+    for (var i = 0; i < registration_elements.length; i++) {
+        registration_elements[i].style.visibility = "visible";
+        registration_elements[i].style.position = "relative";
+    };
+    for (var i = 0; i < login_elements.length; i++) {
+        login_elements[i].style.visibility = "hidden";
+        login_elements[i].style.position = "absolute";
+    };
+};
+function login_div() {
+    var registration_elements = document.getElementsByClassName("registration");
+    var login_elements = document.getElementsByClassName("login");
+    for (var i = 0; i < registration_elements.length; i++) {
+        registration_elements[i].style.visibility = "hidden";
+        registration_elements[i].style.position = "absolute";
+    };
+    for (var i = 0; i < login_elements.length; i++) {
+        login_elements[i].style.visibility = "visible";
+        login_elements[i].style.position = "relative";
+    };
+};
